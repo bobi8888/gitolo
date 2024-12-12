6b48fa4016f079bd5c0ecbab5edcbcaedb088c64
chris <cjaler@gmail.com>
2024-12-12 12:41:42 -0600

    added utils.h

--- olo/sfmlTemplate/olo.cpp
+++ olo/sfmlTemplate/olo.cpp
@@ -1,10 +1,5 @@
 
-#include <iostream>
-#include <SFML/System.hpp>
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/OpenGL.hpp>
-#include <SFML/Audio.hpp>
+#include "utils.h"
 
 int main()
 {
